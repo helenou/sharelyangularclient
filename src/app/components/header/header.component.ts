ea2f0126af62783dedcacbd0a6f6a40727c66407
Thais Révillon <trevillon@jehann.fr>
2020-01-16 15:04:22 +0100

    Login / Register en modal OK ( vérifier si ça fonctionne ! )

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {AuthService} from '../../services/auth.service';
 
@@ -6,8 +6,18 @@ import {AuthService} from '../../services/auth.service';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
+
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('pillsLogin', {static: false}) pillsLogin: ElementRef;
+  @ViewChild('pillsRegister', {static: false}) pillsRegister: ElementRef;
+  @ViewChild('navLogin', {static: false}) navLogin: ElementRef;
+  @ViewChild('pillsRegister', {static: false}) navRegister: ElementRef;
+
+  classActiveLogin = '';
+  classActiveRegister = '';
+  classShowRegister = '';
+  classShowLogin = '';
 
   constructor( private titleService: Title, private authService: AuthService) { }
 
@@ -18,5 +28,19 @@ export class HeaderComponent implements OnInit {
 
   setPageTitle(title: string) {
     this.titleService.setTitle(title);
+  }
+
+  openModal(choice) {
+    if (choice === 'login') {
+        this.classActiveLogin = 'active';
+        this.classShowRegister = '';
+        this.classShowLogin = 'show';
+        this.classActiveRegister = '';
+    } else {
+        this.classActiveRegister = 'active';
+        this.classActiveLogin = '';
+        this.classShowLogin = '';
+        this.classShowRegister = 'show';
+    }
   }
 }
