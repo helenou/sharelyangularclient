0f7a8db7e05ba27264fc38b8c6852d5c7fddc153
helenou <ln.lejeune@gmail.com>
2020-01-17 09:53:26 +0100

    add register component

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService, } from '../../services/alert.service';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -12,10 +17,24 @@ export class RegisterComponent implements OnInit {
   loading = false;
   submitted = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {
+  error: {};
+  registerError: string;
 
+  nom: string;
+  prenom: string;
+  email: string;
+  motDePasse: string;
+
+ constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthService,
+    private userService: UserService,
+    private alertService: AlertService
+  ) {
+    if (this.authenticationService.isLoggedIn() === true) { // à vérifier état du currentUser
+      this.router.navigate(['/info']); // naviguer vers l'écran tableau de bord utilisateur
+    }
   }
 
   ngOnInit() {
@@ -27,5 +46,26 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  onSubmit() {}
+  onSubmit() { // TODO check the below
+      this.submitted = true;
+
+      this.alertService.clear();
+
+      if (this.registerForm.invalid) {
+        return;
+      }
+
+      this.loading = true;
+      this.userService.register(this.registerForm.value)
+        .pipe(first())
+        .subscribe(
+          data => {
+            this.alertService.success('Registration successful', true);
+            this.router.navigate(['/login']);
+          },
+          error => {
+            this.alertService.error(error);
+            this.loading = false;
+          });
+    }
 }
