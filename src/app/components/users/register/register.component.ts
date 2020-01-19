f41880844aadaad9267a3a6da69445b496e48caa
houdheyfa <houdheyfa.talib@gmaiL.com>
2020-01-19 22:38:08 +0100

    Update register.component.ts
    Merci de bien vouloir pushé et commité que des choses fonctionnels !

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -3,7 +3,6 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../../services/user.service';
 import { AuthService } from '../../../services/auth.service';
-//import { AlertService } from '../../../services/alert.service';
 
 @Component({
   selector: 'app-register',
@@ -13,66 +12,41 @@ import { AuthService } from '../../../services/auth.service';
 
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
-  //loading = false;
-  //submitted = false;
 
   error: {};
   registerError: string;
-
-
-
  constructor(
     private fb: FormBuilder,
     private router: Router,
     private userService: UserService,
     private authService: AuthService,
-    //private alertService: AlertService
   )  {}
-  //{
- //   if (this.authenticationService.isLoggedIn() === true) { // à vérifier état du currentUser
- //     this.router.navigate(['/info']); // naviguer vers l'écran tableau de bord utilisateur
- //   }
- // }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      nom: ['', Validators.required],
-      prenom: ['', Validators.required],
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
       email: ['', Validators.required],
-      motDePasse: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
-/*   get nom() { return this.registerForm.get('nom'); }
-  get prenom() { return this.registerForm.get('prenom'); }
-  get email() { return this.registerForm.get('email'); }
-  get motDePasse() { return this.registerForm.get('motDePasse'); } */
-  
   get lastName() { return this.registerForm.get('lastName'); }
   get firstName() { return this.registerForm.get('firstName'); }
   get email() { return this.registerForm.get('email'); }
   get password() { return this.registerForm.get('password'); }
 
   onSubmit() {
-      //this.submitted = true;
-
-      //this.alertService.clear();
 
       if (this.registerForm.invalid) {
-        return "Cannot create user, check the fields";
+        return 'Cannot create user, check the fields';
       }
 
-      //this.loading = true;
       this.userService.create(this.registerForm.value).subscribe((data) => {
           if (this.authService.isLoggedIn() === true) {
-            console.log("user already logged in. log out to create new one.");
-            const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/login';
-            
-            //this.alertService.success('Registration successful', true);
-	        } else {
-          //this.registerError =
-          //return  'Impossible de vous enregistrer.';
-          console.log("user creation, it should be created here - if no error");
+            console.log('user already logged in. log out to create new one.');
+            const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/login'; } else {
+          console.log('user creation, it should be created here - if no error');
           this.registerError = 'Impossible d\'ajouter l\'utilisateur.';
         }
       },
@@ -81,14 +55,3 @@ export class RegisterComponent implements OnInit {
   }
 }
 
-
-////        }
-	    
-////          },
-////          error => {
-////            this.alertService.error(error);
-            //this.loading = false;
-////           }
-////	  );
-////    }
-////}
